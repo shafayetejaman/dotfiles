3b26c8439bbc49e796801ed9237fcf5fbf40eae4
shafayetejaman <shafaitzaman@gmail.com>
2026-02-18 12:06:59 +0600

    fixed zed keymaps

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -1,11 +1,19 @@
 return {
   {
     "nvim-mini/mini.surround",
+    event = "VeryLazy",
+    silent = true,
     opts = {
       mappings = {
         delete = "ds",
         replace = "cs",
         add = "ys"
+      },
+      custom_surroundings = {
+        ["("] = { output = { left = "(", right = ")" } },
+        ["{"] = { output = { left = "{", right = "}" } },
+        ["["] = { output = { left = "[", right = "]" } },
+        ["<"] = { output = { left = "<", right = ">" } },
       },
     },
   },

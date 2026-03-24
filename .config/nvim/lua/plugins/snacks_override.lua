7447825b011fb5b6aa6aa970d3daa2da8a2b1688
shafayetejaman <shafaitzaman@gmail.com>
2026-03-24 18:23:06 +0600

    fix nvim notify

--- .config/nvim/lua/plugins/snacks_override.lua
+++ .config/nvim/lua/plugins/snacks_override.lua
@@ -1,24 +1,29 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      words = { enabled = false },
-      terminal = {
-        win = {
-          position = "float",
-          border = "single",
-        },
-      },
-      picker = {
-        win = {
-          input = {
-            keys = {
-              ["J"] = { "preview_scroll_down", mode = { "n" } },
-              ["K"] = { "preview_scroll_up", mode = { "n" } },
+    {
+        "folke/snacks.nvim",
+        opts = {
+            words = { enabled = false },
+            notifier = {
+                enabled = true,
+                level = vim.log.levels.WARN, -- only show WARN and ERROR
+                -- level = vim.log.levels.ERROR, -- only show real errors (hides progress too)
             },
-          },
+            terminal = {
+                win = {
+                    position = "float",
+                    border = "single",
+                },
+            },
+            picker = {
+                win = {
+                    input = {
+                        keys = {
+                            ["J"] = { "preview_scroll_down", mode = { "n" } },
+                            ["K"] = { "preview_scroll_up", mode = { "n" } },
+                        },
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }

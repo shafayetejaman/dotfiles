2c323bc3e04aa425723f73d4fac1eeb750223131
shafayetejaman <shafaitzaman@gmail.com>
2026-07-04 19:50:42 +0600

    fix nvim treeseter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,16 +16,18 @@ require("neo-tree").setup({
       },
       ["l"] = "open",
       ["h"] = "open",
+      ["<Left>"] = "open",
+      ["<Right>"] = "open",
       ["s"] = "open_split",
       ["v"] = "open_vsplit",
-    }
+    },
   },
   filesystem = {
     filtered_items = {
       hide_dotfiles = false,
     },
     follow_current_file = {
-      enabled = true,          -- This auto-focuses the current file in the tree whenever the active buffer changes
+      enabled = true, -- This auto-focuses the current file in the tree whenever the active buffer changes
       leave_dirs_open = false, -- Optional: If false, auto-expanded directories will close after focusing (keeps the tree tidy)
     },
   },
@@ -33,11 +35,11 @@ require("neo-tree").setup({
     {
       event = "neo_tree_buffer_enter",
       handler = function(arg)
-        vim.cmd [[
+        vim.cmd([[
           setlocal relativenumber
-        ]]
+        ]])
       end,
-    }
+    },
   },
   -- Optionally for the buffers source (if you use it):
   buffers = {
@@ -45,5 +47,4 @@ require("neo-tree").setup({
       enabled = true,
     },
   },
-
 })

2448621450e449345c90ef96b15f46f3cf9a0622
shafayetejaman <shafaitzaman@gmail.com>
2026-07-17 14:46:42 +0600

    fic

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,48 +3,48 @@ require("config.lazy")
 require("plugins.assistant")
 
 require("neo-tree").setup({
-  window = {
-    mappings = {
-      ["P"] = {
-        "toggle_preview",
-        config = {
-          use_float = true,
-          -- use_image_nvim = true,
-          -- use_snacks_image = true,
-          -- title = 'Neo-tree Preview',
+    window = {
+        mappings = {
+            ["P"] = {
+                "toggle_preview",
+                config = {
+                    use_float = true,
+                    -- use_image_nvim = true,
+                    -- use_snacks_image = true,
+                    -- title = 'Neo-tree Preview',
+                },
+            },
+            ["l"] = "open",
+            ["h"] = "open",
+            ["<Left>"] = "open",
+            ["<Right>"] = "open",
+            ["-"] = "open_split",
+            ["\\"] = "open_vsplit",
         },
-      },
-      ["l"] = "open",
-      ["h"] = "open",
-      ["<Left>"] = "open",
-      ["<Right>"] = "open",
-      ["s"] = "open_split",
-      ["v"] = "open_vsplit",
     },
-  },
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
+    filesystem = {
+        filtered_items = {
+            hide_dotfiles = false,
+        },
+        follow_current_file = {
+            enabled = true, -- This auto-focuses the current file in the tree whenever the active buffer changes
+            leave_dirs_open = false, -- Optional: If false, auto-expanded directories will close after focusing (keeps the tree tidy)
+        },
     },
-    follow_current_file = {
-      enabled = true, -- This auto-focuses the current file in the tree whenever the active buffer changes
-      leave_dirs_open = false, -- Optional: If false, auto-expanded directories will close after focusing (keeps the tree tidy)
-    },
-  },
-  event_handlers = {
-    {
-      event = "neo_tree_buffer_enter",
-      handler = function(arg)
-        vim.cmd([[
+    event_handlers = {
+        {
+            event = "neo_tree_buffer_enter",
+            handler = function(arg)
+                vim.cmd([[
           setlocal relativenumber
         ]])
-      end,
+            end,
+        },
     },
-  },
-  -- Optionally for the buffers source (if you use it):
-  buffers = {
-    follow_current_file = {
-      enabled = true,
+    -- Optionally for the buffers source (if you use it):
+    buffers = {
+        follow_current_file = {
+            enabled = true,
+        },
     },
-  },
 })
--- .config/nvim/lua/plugins/snacks_override.lua
+++ .config/nvim/lua/plugins/snacks_override.lua
@@ -1,29 +1,31 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      notifier = {
-        enabled = true,
-        level = vim.log.levels.WARN,         -- only show WARN and ERROR
-        -- level = vim.log.levels.ERROR, -- only show real errors (hides progress too)
-      },
-      words = { enabled = false },
-      terminal = {
-        win = {
-          position = "float",
-          border = "single",
-        },
-      },
-      picker = {
-        win = {
-          input = {
-            keys = {
-              ["J"] = { "preview_scroll_down", mode = { "n" } },
-              ["K"] = { "preview_scroll_up", mode = { "n" } },
+    {
+        "folke/snacks.nvim",
+        opts = {
+            words = { enabled = false },
+            notifier = {
+                enabled = true,
+                level = vim.log.levels.WARN, -- only show WARN and ERROR
+                -- level = vim.log.levels.ERROR, -- only show real errors (hides progress too)
             },
-          },
+            terminal = {
+                win = {
+                    position = "float",
+                    border = "single",
+                },
+            },
+            picker = {
+                win = {
+                    input = {
+                        keys = {
+                            ["J"] = { "preview_scroll_down", mode = { "n" } },
+                            ["K"] = { "preview_scroll_up", mode = { "n" } },
+                            ["-"] = { "edit_split", mode = { "n" } },
+                            ["\\"] = { "edit_vsplit", mode = { "n" } },
+                        },
+                    },
+                },
+            },
         },
-      },
     },
-  },
 }

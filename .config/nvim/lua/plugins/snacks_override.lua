be3f6ef184fce95fdcdded68150c45d9fa75dc80
shafayetejaman <shafaitzaman@gmail.com>
2026-08-06 21:08:25 +0600

    fix tmux and cleanup nvim

--- .config/nvim/lua/plugins/snacks_override.lua
+++ .config/nvim/lua/plugins/snacks_override.lua
@@ -1,20 +1,20 @@
 return {
     {
         "folke/snacks.nvim",
-        opts = {
-            words = { enabled = false },
-            notifier = {
+        opts = function(_, opts)
+            opts.words = { enabled = false }
+            opts.notifier = {
                 enabled = true,
                 level = vim.log.levels.WARN, -- only show WARN and ERROR
                 -- level = vim.log.levels.ERROR, -- only show real errors (hides progress too)
-            },
-            terminal = {
+            }
+            opts.terminal = {
                 win = {
                     position = "float",
                     border = "single",
                 },
-            },
-            picker = {
+            }
+            opts.picker = {
                 win = {
                     input = {
                         keys = {
@@ -25,7 +25,31 @@ return {
                         },
                     },
                 },
-            },
-        },
+            }
+
+            -- make sure dashboard/preset exists
+            opts.dashboard = opts.dashboard or {}
+            opts.dashboard.preset = opts.dashboard.preset or {}
+            opts.dashboard.preset.keys = opts.dashboard.preset.keys or {}
+
+            -- insert *after* the Lazy button
+            -- find index of Lazy shortcut
+            local insert_at = 1
+            for i, entry in ipairs(opts.dashboard.preset.keys) do
+                if entry.key == "l" then
+                    insert_at = i + 1
+                    break
+                end
+            end
+
+            table.insert(opts.dashboard.preset.keys, insert_at, {
+                icon = "󰺵 ",
+                key = "L",
+                desc = "LeetCode",
+                action = ":Leet",
+            })
+
+            return opts
+        end,
     },
 }

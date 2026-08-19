da760dec4d52213d173d6f6cf86b0619664896f2
shafayetejaman <shafaitzaman@gmail.com>
2026-08-19 15:23:54 +0600

    fix

--- install-pkg.sh
+++ install-pkg.sh
@@ -4,11 +4,10 @@
 # Official repository packages
 sudo pacman -S --needed --noconfirm \
     7zip atuin duf ghostty gsimplecal gsmartcontrol \
-    gst-libav gst-plugins-good gthumb haruna intel-ucode kanata \
-    libdvdcss libmpeg2 libva-utils lsof noto-fonts-extra ntfs-3g \
-    ntfsprogs omarchy-chromium omarchy-keyring omarchy-zsh opencode \
+    gthumb haruna kanata \
+    lsof noto-fonts-extra ntfs-3g omarchy-zsh \
     reflector sshfs stow sudo telegram-desktop terminus-font thefuck \
-    trash-cli ttf-cascadia-mono-nerd ttf-firacode-nerd wl-clip-persist \
+    trash-cli wl-clip-persist \
     yazi yq zed
 
 # AUR packages

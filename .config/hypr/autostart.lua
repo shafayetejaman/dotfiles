d281e492b896dadf75ede2e1aea13ff92a19a90a
shafayetejaman <shafaitzaman@gmail.com>
2026-08-21 11:59:37 +0600

    fix more bugs and refector

--- .config/hypr/autostart.lua
+++ .config/hypr/autostart.lua
@@ -2,10 +2,9 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("python3 /home/shafayet/Work/shutdownTimer/1.0.py")
 	hl.exec_cmd("zen-browser", { workspace = "2 silent" })
-	hl.exec_cmd("ghostty", { workspace = "3 silent" })
-	hl.exec_cmd("sleep 1 && hyprctl dispatch workspace 3")
+	hl.exec_cmd("ghostty", { workspace = "3" })
 	hl.exec_cmd("omarchy-theme-bg-next")
 	-- hl.exec_cmd("wl-clip-persist --clipboard regular")
 	-- Persistent workspace on HDMI-A-1
-	hl.exec_cmd("hyprctl keyword workspace 3,persistent,monitor:HDMI-A-1")
+	-- hl.exec_cmd("hyprctl keyword workspace 3,persistent,monitor:HDMI-A-1")
 end)
--- .config/hypr/input.lua
+++ .config/hypr/input.lua
@@ -3,35 +3,31 @@
 
 -- Keyboard layout and options.
 hl.config({
-  input = {
-    kb_layout = "us",
-    kb_options = "compose:caps",
-    repeat_rate = 40,
-    repeat_delay = 600,
-    numlock_by_default = true,
-
-    touchpad = {
-      scroll_factor = 0.4,
-    },
-  },
+	input = {
+		kb_layout = "us",
+		kb_options = "compose:caps",
+		repeat_rate = 40,
+		repeat_delay = 600,
+		numlock_by_default = true,
+	},
 })
 
--- App-specific touchpad scroll speeds.
-o.window("(Alacritty|kitty|foot)", { scroll_touchpad = 1.5 })
-o.window("com.mitchellh.ghostty", { scroll_touchpad = 0.2 })
-o.window("(microsoft-edge|chromium)", { scroll_touchpad = 0.2 })
-
--- Enable touchpad gestures for changing workspaces.
-hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
-
--- Enable touchpad gestures for moving focus (helpful on scrolling layout).
-hl.gesture({ fingers = 3, direction = "top", action = function() hl.dispatch(hl.dsp.focus({ direction = "l" })) end })
-hl.gesture({ fingers = 3, direction = "bottom", action = function() hl.dispatch(hl.dsp.focus({ direction = "r" })) end })
+-- -- App-specific touchpad scroll speeds.
+-- o.window("(Alacritty|kitty|foot)", { scroll_touchpad = 1.5 })
+-- o.window("com.mitchellh.ghostty", { scroll_touchpad = 0.2 })
+-- o.window("(microsoft-edge|chromium)", { scroll_touchpad = 0.2 })
+--
+-- -- Enable touchpad gestures for changing workspaces.
+-- hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
+--
+-- -- Enable touchpad gestures for moving focus (helpful on scrolling layout).
+-- hl.gesture({ fingers = 3, direction = "top", action = function() hl.dispatch(hl.dsp.focus({ direction = "l" })) end })
+-- hl.gesture({ fingers = 3, direction = "bottom", action = function() hl.dispatch(hl.dsp.focus({ direction = "r" })) end })
 
 -- Misc settings
 hl.config({
-  misc = {
-    focus_on_activate = false,
-    initial_workspace_tracking = false,
-  },
+	misc = {
+		focus_on_activate = false,
+		initial_workspace_tracking = false,
+	},
 })

b985d3352d3dfaeca7c66902de2026bbf4369ef2
shafayetejaman <shafaitzaman@gmail.com>
2026-08-24 21:46:32 +0600

    fix float rule

--- .config/hypr/looknfeel.lua
+++ .config/hypr/looknfeel.lua
@@ -19,6 +19,12 @@ hl.config({
 -- Smart single-window borders removal
 hl.workspace_rule({ workspace = "w[tv1]", border_size = 0 })
 hl.workspace_rule({ workspace = "f[1]", border_size = 0 })
+hl.window_rule({
+	match = {
+		float = true,
+	},
+	border_size = 2,
+})
 
 -- Opacity overrides for specific apps
 o.window("zen", { opacity = "1.0 1.0" })
@@ -27,9 +33,9 @@ o.window("dev.zed.Zed", { opacity = "1.0 1.0" })
 o.window("com.mitchellh.ghostty", { opacity = "1.0 1.0" })
 
 -- Float and size rules
-o.window("org.gnome.gThumb", { float = true, border_size = 2, size = { 900, 600 } })
-o.window("org.kde.haruna", { float = true, border_size = 2, size = { 900, 600 } })
-o.window("org.localsend.localsend_app", { float = true, border_size = 2, size = { 900, 700 } })
+o.window("org.gnome.gThumb", { float = true, size = { 900, 600 } })
+o.window("org.kde.haruna", { float = true, size = { 900, 600 } })
+o.window("org.localsend.localsend_app", { float = true, size = { 900, 700 } })
 
 -- Cursor theme
 hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
